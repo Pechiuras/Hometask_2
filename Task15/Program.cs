61824e2df51a97ab8c8d07d9f1e16c4d3af11ab5
Aleksandr Pechiuras <alpechura@yandex.ru>
2023-02-01 12:21:23 +0500

    Task15

---
+++ Task15/Program.cs
@@ -0,0 +1,30 @@
+﻿//Ввод дня недели и проверка является ли это день выходным
+Console.Clear();
+Console.Write("Введите день недели: ");
+int n = Convert.ToInt32(Console.ReadLine());
+while ( n < 1 || n > 7)
+{
+ Console.Write("Вы ошибились!\nВведите день недели: ");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+
+if (n == 1)
+Console.WriteLine("Нет. Это рабочий день недели");
+
+else if (n == 2)
+Console.WriteLine("Нет. Это рабочий день недели");
+
+else if (n == 3)
+Console.WriteLine("Нет. Это рабочий день недели");
+
+else if (n == 4)
+Console.WriteLine("Нет. Это рабочий день недели");
+
+else if (n==5)     
+Console.WriteLine("Нет. Это рабочий день недели");
+
+else if (n==6)
+Console.WriteLine("Да. Это Выходной");
+
+else if (n==7)
+Console.WriteLine("Да. Это Выходной");
