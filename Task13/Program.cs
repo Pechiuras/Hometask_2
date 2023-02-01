c15d6999306e6a630135145d02dc27e102c6027f
Aleksandr Pechiuras <alpechura@yandex.ru>
2023-02-01 17:55:07 +0500

    Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,18 @@
+﻿//Вывод третьей цифры заданного числа или вывод сообщения, что 3 цифры нет
+Console.Clear();
+Console.Write ("Введите числож ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n < 100)
+Console.WriteLine("Третьей цифры нет");
+else
+{
+    while (n >= 999)
+    {
+         n = n / 10;
+    }
+     Console.WriteLine(n);
+     Console.WriteLine(n % 10);
+}
+
+
+
