08ed83de44c0ae7739b0712297e0e9bd0a33a9de
Aleksandr Pechiuras <alpechura@yandex.ru>
2023-02-01 18:19:45 +0500

    Дополнительная задача

---
+++ ad_Task/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine()), max1 = n, max2 = 0;
+while (n != 0)
+{
+  n = Convert.ToInt32(Console.ReadLine());
+  if (n > max1)
+  {
+     max2 = max1;
+     max1 = n;
+  }
+  else if (n > max2)
+  max2 = n;
+
+} 
+
+Console.WriteLine(max2);
+
+
+
+
+
+
