405e9e4bca4a02aac851274ca7bf0e0ab912a230
Aleksandr Pechiuras <alpechura@yandex.ru>
2023-02-01 17:12:57 +0500

    Изменеия в коде

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -2,6 +2,5 @@
 Console.Clear();
 int n = new Random().Next(100, 1000); // [100, 999]
 Console.WriteLine($"Случайное число: {n}"); 
-int n1 = ((n / 10) % 10);
-//int n3 = n % 10;
-Console.WriteLine(n1);
+int n2 = ((n / 10) % 10);
+Console.WriteLine(n2);
